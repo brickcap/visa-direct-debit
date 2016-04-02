45ac0d58e98f5b44b44738ce3e41aaf84cc63106
brickcap <akshatjiwan@gmail.com>
2016-04-02 12:58:45 +0530

    Finally visa says hello :)

--- lua/hello_world.lua
+++ lua/hello_world.lua
@@ -5,8 +5,13 @@ if test then
 else
    uname_prod = config.username_prod..":"..config.password_prod
 end
+
 local base_64_auth_headers = "Basic "..ngx.encode_base64(uname_pass)
+ngx.log(ngx.ERR,base_64_auth_headers)
 ngx.req.set_header("Authorization",base_64_auth_headers)
+ngx.req.set_header("Content-Type","application/json")
+ngx.req.set_header("Accept","application/json")
+
 local hello_res = ngx.location.capture("/visa_sandbox/vdp/helloworld")
 ngx.status =hello_res.status
 ngx.say(hello_res.body)
