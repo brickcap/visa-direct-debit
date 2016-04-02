f733e7817f9778908613d0ec254223805e1c8143
brickcap <akshatjiwan@gmail.com>
2016-04-02 15:29:52 +0530

    Some refactoring

--- lua/hello_world.lua
+++ lua/hello_world.lua
@@ -1,8 +1,5 @@
-local base_64_auth_headers = "Basic "..ngx.encode_base64(uname_pass)
-ngx.req.set_header("Authorization",base_64_auth_headers)
-ngx.req.set_header("Content-Type","application/json")
-ngx.req.set_header("Accept","application/json")
-
+local helpers = require("lua/helpers")
+helpers.set_common_headers()
 local hello_res = ngx.location.capture("/visa_sandbox/vdp/helloworld")
 ngx.status =hello_res.status
 ngx.say(hello_res.body)
