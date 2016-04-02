1f329f2ec6c77fd404a11730dd8f1ccdb00c7dae
brickcap <akshatjiwan@gmail.com>
2016-04-02 16:49:58 +0530

    If invalid currecny code is being called return it

--- lua/access/acc_pull_funds_transfers.lua
+++ lua/access/acc_pull_funds_transfers.lua
@@ -1,6 +1,6 @@
 local helpers = require("helpers")
 local validate = require("lua/lua-resty-validation/lib/resty/validation")
-local country_codes = require("lua/access/country_codes")
+local country_codes,currency_codes = require("lua/access/country_codes"),require("lua/access/currency_codes")
 -- {
 --   "acquirerCountryCode"= "840",
 --   "acquiringBin": "408999",
@@ -29,7 +29,8 @@ local country_codes = require("lua/access/country_codes")
 -- }
 ngx.req.read_body()
 local post_args = ngx.req.get_post_args()
-if post_args.acquiringBin=='' or post_args.acquiringBin == nil then
+
+if not validate:len(6,11)(post_args.acquiringBin) then
    ngx.status = ngx.HTTP_BAD_REQUEST
    ngx.say("Invalid acquiringBin")
 end
@@ -38,6 +39,21 @@ if not helpers.has_value(country_codes,post_args.acquirerCountryCode) then
    ngx.say("Invalid acquirerCountryCode ")
 end
 
-local systemsTraceAuditNumber = math.random(0,999999)
-local retrievalReferenceNumber = helpers.get_retreival_ref_no(systemsTraceAuditNumber)
-local localTransactionDateTime = os.date("%Y-%m-%dT%H:%M:%S")
+if validate:len(13,19)(post_args.senderPrimaryAccountNumber) then 
+   ngx.status = ngx.HTTP_BAD_REQUEST
+   ngx.say("Invalid senderPrimaryAccountNumber")
+end
+
+if post_args.senderCardExpiryDate=='' or post_args.senderCardExpiryDate==nil then 
+   ngx.status = ngx.HTTP_BAD_REQUEST
+   ngx.say("Invalid senderCardExpiryDate")
+end
+
+if not helpers.has_value(currency_codes,post_args.senderCurrencyCode) then
+   ngx.status = ngx.HTTP_BAD_REQUEST
+   ngx.say("Invalid senderCardExpiryDate")
+end
+
+post_args.systemsTraceAuditNumber = math.random(0,999999)
+post_args.retrievalReferenceNumber = helpers.get_retreival_ref_no(systemsTraceAuditNumber)
+post_args.localTransactionDateTime = os.date("%Y-%m-%dT%H:%M:%S")
