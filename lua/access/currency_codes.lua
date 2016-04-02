0d1eb95ca324a08cbd6da3c6eb3e9e026956fc59
brickcap <akshatjiwan@gmail.com>
2016-04-02 16:47:41 +0530

    Currency Code matrix is ready

---
+++ lua/access/currency_codes.lua
@@ -0,0 +1,30 @@
+local _M = {}
+
+_M.currency_codes = {
+"ALL","DZD","USD","EUR","AOA","XCD","NOK",
+"XCD","ARS","AMD","AWG","AUD","EUR","AZN","BSD","BHD","BDT","BBD","BYR",
+"EUR","BZD","XOF","BMD","INR",
+"BOB","BAM","BWP","NOK","BRL",
+"USD","BND","BGN","XOF","BIF","KHR","XAF",
+"CAD","CVE","KYD","XAF","CLP",
+"CNY","AUD","COP","KMF","XAF","NZD","CRC","XOF","HRK","CUP","EUR","CZK","CDF",
+"DKK","DJF","XCD","DOP","USD","EGP","USD","XAF",
+"ERN","EUR","ETB","FKP","DKK","FJD","EUR","XPF","EUR",
+"XAF","GMD","GEL","EUR","GHS","GIP",
+"EUR","DKK","XCD","EUR","USD","GTQ","GNF","XOF","GYD","HTG","EUR","HNL","HKD","HUF",
+"ISK","INR","IDR","IRR","IQD","EUR","ILS","EUR","JMD","JPY","JOD","KZT","KES",
+"AUD","KRW","KPW","KWD","KGS","LAK","EUR","LBP","LSL","LRD","LYD",
+"CHF","LTL","EUR","MOP","MKD","MGA","MWK","MYR","MVR","XOF",
+"EUR","USD","EUR","MRO","MUR","EUR","MXN","USD","MDL","EUR","MNT",
+"XCD","EUR","MAD","MZN","MMK",
+"NAD","AUD","NPR","EUR","ANG","XPF","NZD","NIO","XOF","NGN","NZD",
+"AUD","USD","NOK","OMR","PKR","USD","PAB","PGK","PYG","PEN","PHP","NZD","PLN",
+"EUR","USD","QAR","EUR","RON","RUB","RWF","WST","EUR","STD","SAR","XOF",
+"RSD","SCR","SLL","SGD","EUR","SBD","SOS","ZAR","GBP","EUR","LKR","SHP","XCD",
+"EUR","XCD","SDD","SRD","NOK","SZL","SEK","CHF","SYP","TWD",
+"TJS","TZS","THB","USD","XOF","NZD","TOP","TTD","TND","TRY","TMT",
+"USD","AUD","UGX","UAH","AED","GBP","USD","UYU","UZS","VUV",
+"VEF","VND","XPF","MAD","YER","ZMW","ZWL"
+}
+
+return _M;
