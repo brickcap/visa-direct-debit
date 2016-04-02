7dc998b42fa458e2d855177e24b71159a74a8373
brickcap <akshatjiwan@gmail.com>
2016-04-02 14:06:30 +0530

    Moving the configuration to their own files

---
+++ lua/init_by_lua_dev.lua
@@ -0,0 +1,3 @@
+cjson,test = require("cjson"),true
+local config = require("lua/config_main").visa_config
+uname_pass = config.username_sand..":"..config.password_sand
---
+++ lua/init_by_lua_prod.lua
@@ -0,0 +1,3 @@
+cjson,test = require("cjson"),false
+local config = require("lua/config_main").visa_config
+uname_prod = config.username_prod..":"..config.password_prod
