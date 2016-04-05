d27b7c2b2b99d90eecf66e6d37b3e2ebdde06b31
brickcap <akshatjiwan@gmail.com>
2016-04-05 09:00:25 +0530

    Payment attributes api done

--- lua/payment_account_attributes_inquiry.lua
+++ lua/payment_account_attributes_inquiry.lua
@@ -1,7 +1,16 @@
+--https://sandbox.api.visa.com/paai/fundstransferattinq/v1/cardattributes/fundstransferinquiry
+
+local post_args = ngx.ctx.post_args
 local payment_attributes_res = ngx.location.capture(
    url.."paai/fundstransferattinq/v1/cardattributes/fundstransferinquiry",
-   {method=ngx.HTTP_POST,body=cjson.encode(ngx.ctx.post_args)})
-ngx.status = payment_attributes_res.status
-ngx.say(payment_attributes_res.body)
+   {method=ngx.HTTP_POST,body=cjson.encode(post_args)})
 
---https://sandbox.api.visa.com/paai/fundstransferattinq/v1/cardattributes/fundstransferinquiry
+local payment_attributes = cjson.decode(payment_attributes_res.body)
+
+payment_attributes.systemsTraceAuditNumber = post_args.systemsTraceAuditNumber
+payment_attributes.retrievalReferenceNumber = post_args.retrievalReferenceNumber
+
+ngx.status = payment_attributes_res.status
+ngx.header.content_type = payment_attributes_res.header["Content-Type"]
+ngx.say(cjson.encode({status="success",data=payment_attributes}))
+
