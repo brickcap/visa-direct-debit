c6d45f2353fd821366fb2c6d130217bd162af974
brickcap <akshatjiwan@gmail.com>
2016-04-05 08:50:48 +0530

    payment attributes inquiry working

--- lua/access/payment_account_attributes_inquiry.lua
+++ lua/access/payment_account_attributes_inquiry.lua
@@ -1,14 +1,16 @@
-local helpers = require("helpers")
+local helpers = require("lua/helpers")
 local validate = require("lua/lua-resty-validation/lib/resty/validation")
 ngx.req.read_body()
-local post_args = ngx.req.get_post_args()
+local body_data = ngx.req.get_body_data()
+local post_args = cjson.decode(body_data)
 post_args.systemsTraceAuditNumber = math.random(0,999999)
-post_args.retrievalReferenceNumber = helpers.get_retreival_ref_no(systemsTraceAuditNumber)
+post_args.retrievalReferenceNumber = helpers.get_retreival_ref_no(post_args.systemsTraceAuditNumber)
 
 if post_args.primaryAccountNumber == nil or post_args.primaryAccountNumber=='' then 
 ngx.status = ngx.HTTP_BAD_REQUEST
 ngx.say("Invalid primaryAccountNumber")
 else
+   helpers.set_common_headers()
    ngx.ctx.post_args = post_args
    ngx.exit(ngx.OK)
 end
--- lua/payment_account_attributes_inquiry.lua
+++ lua/payment_account_attributes_inquiry.lua
@@ -1,1 +1,7 @@
-local payment_attributes_res = ngx.location.capture()
+local payment_attributes_res = ngx.location.capture(
+   url.."paai/fundstransferattinq/v1/cardattributes/fundstransferinquiry",
+   {method=ngx.HTTP_POST,body=cjson.encode(ngx.ctx.post_args)})
+ngx.status = payment_attributes_res.status
+ngx.say(payment_attributes_res.body)
+
+--https://sandbox.api.visa.com/paai/fundstransferattinq/v1/cardattributes/fundstransferinquiry
